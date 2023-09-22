6ce0b9f4c3f365fa347ac2d51ff3255eaeea7bb1
Daniel Szunomar <daniel.szunomar@gmail.com>
2023-09-22 17:58:54 +0100

    feat: add node docker container, env management and example

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 import express from "express";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
+
+const port = parseInt(process.env.PORT || 3000);
 app.get("/", function (req, res) {
   res.send("Hello World!");
 });
-app.listen(3000, function () {
-  console.log("Example app listening on port 3000!");
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
 });
